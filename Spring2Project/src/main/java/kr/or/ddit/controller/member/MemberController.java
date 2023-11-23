daae6f0fd6906f198e8831fc5812ca69383b3e74
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-11-23 12:42:15 +0900

    1123

--- Spring2Project/src/main/java/kr/or/ddit/controller/member/MemberController.java
+++ Spring2Project/src/main/java/kr/or/ddit/controller/member/MemberController.java
@@ -1,8 +1,10 @@
 package kr.or.ddit.controller.member;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -36,5 +38,88 @@ public class MemberController {
 		return "member/registerForm";
 		
 	}
+	
+	// 1) URL 경로 상의 쿼리 파라미터 정보로부터 요청 데이터를 취득할 수 있다.
+	// a버튼이므로 get 방식!! post 방식 안됨
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public String registerByParameter(String userId, String password) {
+		log.info("registerByParameter() 실행..!");
+		log.info("userId : "+userId);
+		log.info("password : "+password);
+		return "success";
+	}
+	
+	// 2) URL 경로 상의 경로 변수로부터 요청 데이터를 취득할 수 있다.
+	@RequestMapping(value = "/register/{userId}", method = RequestMethod.GET)
+	public String registerByPath(@PathVariable String userId) {
+		//넘겨받은 userId를 경로상에서 꺼낼 때, @PathVariable를 이용한다.
+		log.info("registerByPath() 실행..!");
+		log.info("userId : "+userId);
+		return "success";
+	}
+	
+	// 3) HTML Form 필드명과 컨트롤러 매개변수명이 일치하면 요청 데이터를 취득할 수 있다.
+	// //순서 바꿔도 데이터는 들어옴, 매개변수 명과 Form태그안에 name명만 일치한다면 순서상관없이 가능
+	// 그리고, 매개변수의 위치는 순서에 상관없고 오직 매개변수명이 일치하면 요청 데이터를 취득할 수 있다. 
+	@RequestMapping(value = "/register01", method=RequestMethod.POST)
+	public String register01(String userId, String password) {
+		log.info("register01() 실행...!");
+		log.info("userId : "+userId);
+		log.info("password : "+password);
+		return "success";
+		
+	}
+	
+	//4) HTML Form 필드 값이 숫자일 경우에는 숫자로 타입 변환되어 데이터를 취득할 수 있다. 	
+	@RequestMapping(value = "/register02", method = RequestMethod.POST)
+	public String register02(String userId, String password, int coin) { 
+		log.info("register02() 실행..!!");
+		log.info("userId : "+userId);
+		log.info("password : "+password);
+		log.info("coin : "+coin);
+		return "success";
+	}
+	
+	
+	
+	/*
+	 * 	3. 요청 데이터 처리 어노테이션
+	 * 
+	 * 		-@PathVariable : URL에서 경로 변수 값을 가져오기 위한 어노테이션
+	 * 		-@RequestParam : 요청 파라미터 값을 가져오기 위한 어노테이션
+	 * 		-@RequestHeader : 요청 헤더 값을 가져오기 위한 어노테이션
+	 * 		-@RequestBody : 요청 본문 내용을 가져오기 위한 어노테이션
+	 * 		-@CookieValue : 쿠키 값을 가져오기 위한 어노테이션
+	 */
+	
+	// 1) URL 경로 상의 경로 변수가 여러 개일 때, @PathVariable 어노테이션을 사용하여 특정한 경로 변수명을 지정해준다.
+	@RequestMapping(value = "/register/{userId}/{coin}", method = RequestMethod.GET)
+	public String registerByPath(
+			@PathVariable("userId") String userId,
+			@PathVariable("coin") int coin
+			) {
+		log.info("registerByPath() 실행...!");
+		log.info("userId : " +userId);
+		log.info("coin : " + coin);
+		return "success";
+		
+		
+	}
+	
+	// 2) @RequestParam 어노테이션을 사용하여 특정한 HTML Form의 필드명을 지정하여 요청을 처리 한다.
+	@RequestMapping(value = "/register0202", method=RequestMethod.POST)
+	public String register0202(
+			@RequestParam("userId") String username, String password, int coin
+			) {		
+		log.info("register0202() 실행..!");
+		log.info("username : " + username);
+		log.info("password: " + password);
+		log.info("coin: " + coin);
+		return "success";
+		
+	}
+	
+	
+	
 	
 }
--- Spring2Project/src/main/webapp/WEB-INF/views/member/registerForm.jsp
+++ Spring2Project/src/main/webapp/WEB-INF/views/member/registerForm.jsp
@@ -13,6 +13,48 @@
 	<h4>2. 요청 처리</h4>
 	<hr/>
 	
+	<p>1) URL 경로 상의 쿼리 파라미터 정보로부터 요청 데이터를 취득할 수 있다.</p>
+	<a href="/register?userId=hongkd&password=1234">Button1</a>
+	<br/>
+	
+	<p>2) URL 경로 상의 경로 변수로부터 요청 데이터를 취득할 수 있다.</p>
+	<a href="register/hongkd">Button2</a>
+	
+	<p>
+		3) HTML Form 필드명과 컨트롤러 매개변수명이 일치하면 요청 데이터를 취득할 수 있다.<br/>	
+	</p>
+	<form action="/register01" method="post">
+		userId : <input type="text" name="userId"/><br/>
+		password : <input type="text" name="password"/><br/>
+		coin : <input type="text" name="coin"/><br/>
+		<input type="submit" value="전송"/>
+	</form>
+	
+	
+	<p>4) HTML Form 필드 값이 숫자일 경우에는 숫자로 타입 변환되어 데이터를 취득할 수 있다. </p>
+	<form action="/register02" method="post">
+		userId : <input type="text" name="userId"/><br/>
+		password : <input type="text" name="password"/><br/>
+		coin : <input type="text" name="coin"/><br/>
+		<input type="submit" value="전송"/>
+	</form>
+	
+	
+	<h4>3. 요청 데이터 처리 어노테이션</h4>
+	<hr/>
+	
+	<p>1) URL 경로 상의 경로 변수가 여러 개일 때, @PathVariable 어노테이션을 사용하여 특정한 경로 변수명을 지정해준다.</p>
+	<a href="/register/hongkd/100">Button3</a><br/>
+	
+	
+	<p>2) @RequestParam 어노테이션을 사용하여 특정한 HTML Form의 필드명을 지정하여 요청을 처리 한다. </p>
+	<form action="/register0202" method="post">
+		userId : <input type="text" name="userId"/><br/>
+		password : <input type="text" name="password"/><br/>
+		coin : <input type="text" name="coin"/><br/>
+		<input type="submit" value="전송"/>
+	</form>
+	
 	
 	
 
