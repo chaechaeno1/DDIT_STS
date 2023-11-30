6d0f60c728b279cf1b229f8200644b3491460db5
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-11-30 09:16:24 +0900

    1130

--- Spring2Project/src/main/java/kr/or/ddit/controller/crud/notice/NoticeRetreiveController.java
+++ Spring2Project/src/main/java/kr/or/ddit/controller/crud/notice/NoticeRetreiveController.java
@@ -1,4 +1,4 @@
-package kr.or.ddit.controller.crud;
+package kr.or.ddit.controller.crud.notice;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
